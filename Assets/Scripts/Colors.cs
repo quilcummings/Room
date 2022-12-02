85e3ed87d74a803f14d94657848fad75cc428364
quilcummings <quil735@gmail.com>
2022-12-02 14:03:32 -0700

    added paint meshes

--- Assets/Scripts/Colors.cs
+++ Assets/Scripts/Colors.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class Colors : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         var meshFilter = GetComponent<MeshFilter>();
-        //Add one color for each vertex, and set them all to white
+        
         Color[] colors = new Color[meshFilter.mesh.vertices.Length];
         for (int i = 0; i < colors.Length; i++)
         {
             colors[i] = Color.white;
-        }        meshFilter.mesh.colors = colors;
+        }        
+        meshFilter.mesh.colors = colors;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
--- Assets/Scripts/RayCast.cs
+++ Assets/Scripts/RayCast.cs
@@ -9,12 +9,31 @@ public class RayCast : MonoBehaviour
     {
         Vector3 fwd = transform.TransformDirection(Vector3.forward) * 10;
 
-        if (Physics.Raycast(transform.position, fwd, 10))
-        {
-            Debug.Log("There is something in front of the object!");
-        }
-
-
         Debug.DrawRay(transform.position, fwd, Color.green);
+
+        Ray ray = new Ray(transform.position, fwd);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            var mesh = hit.collider.GetComponent<MeshFilter>().mesh;
+            Vector3 hitPoint = hit.point;
+            hitPoint = hit.transform.InverseTransformPoint(hitPoint);
+            
+            Vector3[] vertices = mesh.vertices;
+            Color[] colors = mesh.colors;
+            
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                var distance = Vector3.Distance(vertices[i], hitPoint);
+                if (distance < 0.1f)
+                {
+                    colors[i] *= 0.7f + (0.3f * distance / 0.2f);
+                }
+            }
+            
+            mesh.colors = colors;
+        }
     }
 }
